bef4f888e87b4620ea17048bfde205472c57b0f5
watanaberyunosuke <harrybwatson@protonmail.com>
2022-02-15 01:03:28 +1100

    module3

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from mainapp.models import BlogPost
+
+admin.site.register(BlogPost)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,34 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import admin
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.http import Http404
-
+from .models import BlogPost
 from datetime import datetime
+
 ALL_POSTS = [
- {
-    'id':0,
-    'author': {'username':'will', 'id':'1'},
-    'title':'My first blog post',
-    'body': 'This is my first post. I hope you like it.',
-    'postdate': datetime(2019, 10, 1),
-    'get_absolute_url': '/blog/post/0'
- },
- {
-    'id':1,
-    'author': {'username':'will', 'id':'1'},
-    'title':'My second blog post',
-    'body': 'I\'ve got tons of these now. This is fun.',
-    'postdate': datetime(2019, 10, 2),
-    'get_absolute_url': '/blog/post/1'
- },
+    {
+        'id': 0,
+        'author': {'username': 'will', 'id': '1'},
+        'title': 'My first blog post',
+        'body': 'This is my first post. I hope you like it.',
+        'postdate': datetime(2019, 10, 1),
+        'get_absolute_url': '/blog/post/0'
+    },
+    {
+        'id': 1,
+        'author': {'username': 'will', 'id': '1'},
+        'title': 'My second blog post',
+        'body': 'I\'ve got tons of these now. This is fun.',
+        'postdate': datetime(2019, 10, 2),
+        'get_absolute_url': '/blog/post/1'
+    },
 ]
 
+
 def index(request):
-    posts = ALL_POSTS
-    return render(request, 'mainapp/index.html', {'posts':posts})
+    posts = BlogPost.objects.all()
+    return render(request, 'mainapp/index.html', {'posts': posts})
+
 
 def post(request, id):
-    try:
-        post = ALL_POSTS[id]
-    except IndexError:
-        raise Http404("Post does not exist")
+    get_object_or_404(BlogPost, pk=id)
     return render(request, 'mainapp/post.html', {'object': post})
